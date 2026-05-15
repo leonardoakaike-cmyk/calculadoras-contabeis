4a4c3e78de3d8ca3f1d46d342e77df81428efb46
leonardoakaike-cmyk <gamestorralvo002@gmail.com>
2026-05-15 12:12:35 -0300

    finalizamos a terceira etapa

--- inss.js
+++ inss.js
@@ -1,10 +1,12 @@
-const salarioBruto = 2500
+const salarioBruto = 3000
 let inss = 0
 
 if(salarioBruto <= 1621){
-   let inss = salarioBruto * 0.075
+   inss = salarioBruto * 0.075
 }else if(salarioBruto > 1621 && salarioBruto <= 2982,84){
-     inss = (salarioBruto - 1621)*0.09 + 121.58
+   inss = (salarioBruto - 1621)*0.09 + 121.58
+}else if(salarioBruto > 2982,84 && salarioBruto <= 4354.27){
+   inss = (salarioBruto - 2982.84)*0.12 + 115.37 + 121.58
 }
 
-console.log(inss)
+console.log(inss.toFixed(2))
